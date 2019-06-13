13be489603bef1a3c9b1b7a962732633f75a13b7
Valentin Mont <valentin.mont@ynov.com>
2019-06-13 15:20:09 +0200

    change

--- Arduino/sensor_arduino/sensor_arduino.ino
+++ Arduino/sensor_arduino/sensor_arduino.ino
@@ -1,3 +1,4 @@
+
 /*This program allows you to read the brightness sensor and display the value on
 thinger.io as well as controlling a led through a present button
 in the dashboard of thinger.io*/
@@ -19,15 +20,12 @@ in the dashboard of thinger.io*/
 #define SSID "A3Pro"                     // set connexion name A3Pro
 #define SSID_PASSWORD "valentin"         // set password connexion valentin
 
-#define INT_PIN 2              // We'll connect the INT pin from our sensor to the
+#define INT_PIN 10              // We'll connect the INT pin from our sensor to the
                                // INT0 interrupt pin on the Arduino.
 
 // make the connection between entered informations and thinger.io
 ThingerWifi thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);  
 APDS9301 apds;
-    
-bool lightIntHappened = false; // flag set in the interrupt to let the
-                               // mainline code know that an interrupt occurred.
 
 /* In this program, we read the DHT11 Humidity and Temperature and if H 30% or T > 30 °C blink red LED.
 We read the SEN0193 Moisture value and if it is very dry (higher 20%), we switch on yellow
@@ -35,19 +33,13 @@ LED. On any other case, we leave the LED green.*/
 
 #include "DHT.h"        // import the library DHT 
 
-#define DHTPIN 3        // initialize the value of the pin at 2    
+#define DHTPIN 2        // initialize the value of the pin at 2    
 #define DHTTYPE DHT11   // initialize the type of DHT 
-#define redPin 10       // initialization of the redPin variable on pine 10
-#define greenPin 9      // initialization of the greenPin variable on pine 9        
-#define bluePin 6       // initialization of the bluePin variable on pine 6 
-#define LED 7           // connect to the PIN 7 to use the LED 
-#define minHt 255       // initialization of the minHt variable and assigned the value 255   
-#define maxHt 510       // initialize the maxHt variable and assign it the value 510 
 
 DHT dht(DHTPIN, DHTTYPE);  
-
-int ledState = 0;       // initialize the value of ledState at 0        
-int ht;                 // initialization of variable ht (soil moisture)            
+       
+int ht;                 // initialization of variable ht (soil moisture)
+int lux;                // initialization of variable lux (luminoux)
 float h      = 0;       // initialization of the variable h and we assign the value 0 (humidity)         
 float t      = 0;       // initialisation of the variable t and we assign the value 0 (temperature)            
 int dry      = 0;       // initialisation of the variable dry and we assign the value 0
@@ -60,14 +52,8 @@ make the connection at thinger.io*/
 void setup() 
 {
   dht.begin();
-  pinMode(LED, OUTPUT);
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
-  dry = 0.8 * (maxHt - minHt) + minHt;
   Serial.begin(115200);                // speed of the sending informations at 115200 bit per second 
   thing.add_wifi(SSID, SSID_PASSWORD); // make the connexion between the source connexion and the thinger.io 
-  pinMode(13, OUTPUT);                 // initialize the led 13 in output
   delay(5);                            // The CCS811 wants a brief delay after startup.
   Wire.begin();                        
     
@@ -105,8 +91,6 @@ void setup()
                                  // a pullup resistor on it as the interrupt is a
                                 // negative going open-collector type output.
   
-  // if the value of lightInt decrease execute the function digitalPinToInterrupt(2)
-  attachInterrupt(digitalPinToInterrupt(2), lightInt, FALLING);
   // print apds.getLowThreshold()
   Serial.println(apds.getLowThreshold());                       
   // print apds.getHighThreshold()    
@@ -120,90 +104,46 @@ of brightness sensor in the console as well as add widgets
 to control the led and to display luminux values on thinger.io*/
 void loop() 
 { 
-  static unsigned long outLoopTimer = 0;
   apds.clearIntFlag();                          
   thing.handle();
-  
-  // led control by thinger.io
-  thing["led"] << digitalPin(13);                        
-      
-  // declare the lux value to use it in the interface of thinger.io
-  thing["LuxValue"] >> outputValue(apds.readCH0Level()); 
+        
+  // declare values to use it in the interface of thinger.io
+  thing["LuxValue"] >> outputValue(apds.readCH0Level());
+  thing["AtmosphericHumidity"] >> outputValue(dht.readHumidity());
+  thing["Celsius"] >> outputValue(dht.readTemperature());
+  thing["SoilMoisture"] >> outputValue(analogRead(0));
      
-  // This is a once-per-second timer that calculates and display the luminux value
-  //  the current lux reading.
-  if (millis() - outLoopTimer >= 1000)
-    {
-      outLoopTimer = millis();
-      Serial.print("Luminous flux: ");
-      Serial.println(apds.readCH0Level(),10);
-
-  if (lightIntHappened)
-    {
-      Serial.println("Interrupt");
-      lightIntHappened = false;    
-    }
-  }
  /*connect sensor to Analog 0*/
-  ht = analogRead(0);  
+  ht = analogRead(0);
+  
+  /*lux reading is assigned to the lux variable*/
+  lux = apds.readCH0Level();
   
   /*moisture reading is assigned to the h variable*/
   h = dht.readHumidity(); 
   
   /*the temperature reading is assigned to the varible t*/ 
   t = dht.readTemperature(); 
-  
-  /*If humidity is below 30% or temperature is above 30°C then blink red LED*/
-  if (t > 30 || h < 30)                    
-  {
-    setColor(255, 0, 0);
-  }
-  /*otherwise if soil moisture variable is greater than dry variable then the led flashes in yellow. */
-  else if (ht > dry)                        
-  {
-    setColor(255, 100, 0);
-  }
-  /*otherwise the led flashes in green*/
-  else                                      
-  {
-    setColor(0, 255, 0);
-  }
-  
-  /*here the value of soil moisture is displayed in the console */
-  Serial.print("Humidity of the earth :");  
-  Serial.print(ht); 
-  Serial.print("    ");
-  delay(100);
-  
+   
   /*If the sensor fails to pick up values then we say "impossible reading"*/
-  if (isnan(t) || isnan(h))                 
+  if (isnan(h) || isnan(t))                 
   {
     Serial.println("Impossible reading !");
   }
   /*otherwise the humidity and temperature values are displayed*/
   else                                     
   {
+    Serial.print(" ");
     Serial.print("Humidity :");
     Serial.print(h);
     Serial.print(" %\t");
     Serial.print("Temperature :");
     Serial.print(t);
     Serial.println("*C");
+    Serial.print("Luminoux flux: ");
+    Serial.println(lux);
+    Serial.print("Humidity of the earth :");  
+    Serial.print(ht);
   }
-  delay(100);    
-}
-
-/*The function lightInt set lightIntHappened at true*/
-void lightInt()
-{
-  lightIntHappened = true; 
-}
-
-/*the setColor function allows the display color 
-desired according to the parameters indicated.*/
-
-void setColor(int redValue, int greenValue, int blueValue) {
-  analogWrite(redPin, redValue);
-  analogWrite(greenPin, greenValue);
-  analogWrite(bluePin, blueValue);
+  delay(1000);    
 }
--- Python/Python_Interface.py
+++ Python/Python_Interface.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import getpass
-
+import time
 
 url_api = "https://api.thinger.io/"
 parametre = "oauth/token"
@@ -12,46 +12,196 @@ main_api = url_api + parametre
 
 monjson = json.dumps([])
 
-mydata = {
+
+sortieduprogramme = 0
+i = 0
+   
+userExist = False
+while userExist == False:
+
+    login = input ("Login : ")
+    psw = getpass.getpass("Password : ")
+    mydata = {
     "Content-Type": "application/x-www-form-urlencoded",
     "grant_type": "password",
-    "username": "valmnt",
-    "password": "valentin13"
+    "username": login,
+    "password": psw
+    }
 
+        
+    validtoken = requests.post(main_api, data= mydata).json()
+    userExist = "access_token" in validtoken
+    
+parametre ="v1/users/valmnt/devices?authorization="+ validtoken["access_token"]
+main_api =url_api + parametre
+object_api = requests.get(main_api, params = mydata)
+    
+while (sortieduprogramme == 0):
+
+    print("Actualisation des devices...")
+            
+    devices = object_api.json()
+
+    print ("Voici la liste des devices disponibles : ")
+    for device in devices:
+        print(device["device"])
+    print("")
+    choice = input ("Quel appareil voulez-vous sélectionner ? ")
+
+    for device in devices:
+        if choice == device["device"]:
+            app = device["device"]
+    print("")
+    sortieduprogramme = 1
+
+tableauPlante = []
+tableau = []
+tableauNomPlante = []
+indePlante = 0
+dico = {}
+x=0
+y=0
+exit=0
+compteurDico=0
+nomPlanteValide=0
+numeroPlant = 0
+choixInformationsValide = "oui"
+verifExit = 0
+
+parametres ="v2/users/valmnt/devices/arduino/LuxValue?authorization="+ validtoken["access_token"]
+main_api =url_api + parametres
+object_api = requests.get(main_api, params=mydata)
+lux = object_api.json()
+
+parametres ="v2/users/valmnt/devices/arduino/AtmosphericHumidity?authorization="+ validtoken["access_token"]
+main_api =url_api + parametres
+object_api = requests.get(main_api, params=mydata)
+ha = object_api.json()
+
+parametres ="v2/users/valmnt/devices/arduino/Celsius?authorization="+ validtoken["access_token"]
+main_api =url_api + parametres
+object_api = requests.get(main_api, params=mydata)
+t = object_api.json()
+
+parametres ="v2/users/valmnt/devices/arduino/SoilMoisture?authorization="+ validtoken["access_token"]
+main_api =url_api + parametres
+object_api = requests.get(main_api, params=mydata)
+ht = object_api.json()
+
+dicoCapteur = {
+    "La luminosite": lux['out'],
+    "La temperature":t['out'],
+    "L'humidite de la terre":ht['out'],
+    "L'humidite de l'air":ha['out'],
 }
 
-validtoken = requests.post(main_api, data= mydata).json()
-connect = 0
-sortieduprogramme = 0
-#print(validtoken)
-while(connect != 1):
-    login = input ("Login : ")
-    psw = getpass.getpass (prompt= "Password : ")
+def AnalyseCapteur(valeurCapteur,valeurRequise):
+    print(valeurCapteur,": ",end="")
+    if dicoCapteur[valeurCapteur] > (valeurRequise*1.2):
+        print(dicoCapteur[valeurCapteur],"est supérieur de plus de 20% par rapport à la valeur requise",valeurRequise)
+        print(valeurCapteur,"est trop haute")
+        
+    elif dicoCapteur[valeurCapteur] > (valeurRequise*1.4):
+        print(dicoCapteur[valeurCapteur],"est supérieur de plus de 40% par rapport à la valeur requise",valeurRequise)
+        print(valeurCapteur,"est beaucoup trop haute")
+        
+    elif (dicoCapteur[valeurCapteur]*1.2) < valeurRequise:
+        print(dicoCapteur[valeurCapteur],"est inférieur d'au moins 20% par rapport à la valeur requise",valeurRequise)
+        print(valeurCapteur,"est trop haute")
+        
+    elif (dicoCapteur[valeurCapteur]*1.4) < valeurRequise:
+        print(dicoCapteur[valeurCapteur],"est inférieur d'au moins 40% par rapport à la valeur requise",valeurRequise)
+        print(valeurCapteur,"est beaucoup trop haute")
 
-    if(requests.get('https://api.github.com/user', auth = (login, psw))):
-        connect = 1
-        while (sortieduprogramme == 0):
-            print("Actualisation des devices...")
-            parametre ="v1/users/valmnt/devices?authorization="+ validtoken["access_token"]
-            main_api =url_api + parametre
-            object_api = requests.get(main_api, params = mydata)
+fichier = open("plantesDonnees.csv",'r')
 
-            devices = object_api.json()
-            print ("Voici la liste des devices disponibles : ")
-            for device in devices:
-                print(device["device"])
-            print("")
-            choice = input ("Quel appareil voulez-vous sélectionner ? ")
+for line in fichier:
+    tableau = [line.split(";")]
+    tableauPlante += tableau
 
-            for device in devices:
-                if choice == device["device"]:
-                    app = device["device"]
-            print("")
-            print ("vous avez sélectionné : ", app)
-            print(object_api.json())
-            sortieduprogramme = 1
+while nomPlanteValide == 0:
+    nomPlanteValide=0
+    while y < len(tableauPlante)-1:
+        x=0
+        while x < len(tableauPlante[y]):
+            if tableauPlante[y][x] == "nom":
+                print(tableauPlante[y][x+1])
+                tableauNomPlante.append(tableauPlante[y][x+1])
+            x=x+1
+        y=y+1
+    x=0
+    y=0
+    numeroPlant = input("Quelle est le nom de la plante que vous voulez chouchouter ? ")
+    for x in range(0,len(tableauNomPlante)):
+        if numeroPlant == tableauNomPlante[x]:
+            print("Vous avez choisis : ",numeroPlant)
+            nomPlanteValide=1
+            break;
+    if nomPlanteValide == 0:
+        print("Cette plante n'existe pas.")
 
-    else:
-        print("")
-        print("Login or password not good")
-        print("")
+while compteurDico <= len(tableauPlante[x])-2:
+    dico[tableauPlante[x][compteurDico]]=tableauPlante[x][compteurDico+1]
+    compteurDico=compteurDico+2
+
+while exit == 0:
+    while choixInformationsValide == "oui":
+        choixInformations = input("Voulez vous voir les besoins de la plante(0), son état actuel(1) ou une description de la plante(2) ?")
+        if choixInformations == "0":
+            print(dico["nom"]," : ")
+            print("Cette plante à besoin d'une humidité ambiante de",dico["humidité de l'air"])
+            print("Cette plante à besoin d'une humidité de sol de",dico["humidité du sol"])
+            print("Cette plante à besoin d'une température minimale de",dico["température min"])
+            print("Cette plante à besoin d'une luminosité de",dico["luminosité"])
+            
+        elif choixInformations == "1":
+            print(dico["nom"]," : ")
+            AnalyseCapteur("La luminosite",float(dico['luminosité']))
+            AnalyseCapteur("La temperature",float(dico["température min"]))
+            AnalyseCapteur("L'humidite de la terre",float(dico["humidité du sol"]))
+            AnalyseCapteur("L'humidite de l'air",float(dico["humidité de l'air"]))
+            
+        elif choixInformations == "2":
+            print(dico["nom"]," : ")
+            print("Cette plante fait partie de la famille des",dico["Famille"])
+            print("Elle vient majoritairement de",dico["Origine"])
+            print("Sa période de fleuraison est",dico["Période de floraison"])
+            print("La couleur des fleurs est",dico["Couleur des fleurs"])
+            print("C'est une",dico["Type de plante"])
+            print("Sa hauteur à maturité est ",dico["Hauteur moyenne"])
+        else:
+            print("Cela ne fait pas partie des choix possibles.")
+
+        verifExit=0
+        while verifExit == 0:
+            choixInformationsValide = str(input("Voulez vous continuer à utiliser l'appli ?(oui ou non)"))
+            print(choixInformationsValide)
+            if (choixInformationsValide != "oui" and choixInformationsValide != "non"):
+                print("Je ne comprends pas.")
+                verifExit = 0
+            elif choixInformationsValide == "oui":
+                verifExit = 1
+            else:
+                verifExit = 1
+                exit = 1
+
+fichier.close()
+
+
+    
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    
