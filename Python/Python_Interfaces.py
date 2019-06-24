aa09883f30db89ec70487ddd3962ea2077aada64
Valentin Mont <valentin.mont@ynov.com>
2019-06-24 16:19:56 +0200

    comment

--- Python/Python_Interfaces.py
+++ Python/Python_Interfaces.py
@@ -16,11 +16,12 @@ main_api = url_api + parametre
 
 monjson = json.dumps([])
 
-
 sortieduprogramme = 0
 i = 0
 alert_error = 0
 userExist = False
+
+        # Système de connexion #
 while userExist == False:
     if(alert_error >= 1):
         print("")
@@ -44,6 +45,7 @@ parametre ="v1/users/" + login + "/devices?authorization="+ validtoken["access_t
 main_api =url_api + parametre
 object_api = requests.get(main_api, params = mydata)
 
+        # Si la connexion a été réussite, on affiche la liste des appareils #
 while (sortieduprogramme == 0):
 
     devices = object_api.json()
@@ -83,6 +85,8 @@ numeroPlant = 0
 choixInformationsValide = "oui"
 verifExit = 0
 
+        # Fonction permettant de comparer les valeurs de chaques #
+        # capteurs par rapport aux conditions idéales #
 def AnalyseCapteur(valeurCapteur,valeurRequise):
     print(valeurCapteur,": ",end="")
     if dicoCapteur[valeurCapteur] > (valeurRequise*1.2):
@@ -101,8 +105,10 @@ def AnalyseCapteur(valeurCapteur,valeurRequise):
         print(dicoCapteur[valeurCapteur],"est inférieur d'au moins 40% par rapport à la valeur requise",valeurRequise)
         print(valeurCapteur,"est beaucoup trop haute")
 
+        # Ouverture du fichier contenant la liste des diffrentes plantes #
 fichier = open("plantesDonnees.csv",'r')
 
+        # Matrice contenant les informations relatives de chaque plante #
 for line in fichier:
     tableau = [line.split(";")]
     tableauPlante += tableau
@@ -119,6 +125,9 @@ while nomPlanteValide == 0:
         y=y+1
     x=0
     y=0
+
+        # Parcours le tableau et compare les infos #
+        # de l'input par rapport à ce tableau #
     numeroPlant = input("Quel est le nom de la plante que vous voulez chouchouter ? ")
     for x in range(0,len(tableauNomPlante)):
         if numeroPlant == tableauNomPlante[x]:
@@ -128,12 +137,14 @@ while nomPlanteValide == 0:
     if nomPlanteValide == 0:
         print("Cette plante n'existe pas.")
 
+        # Déplacement des informations du tableau dans un dictionnaire #
 while compteurDico <= len(tableauPlante[x])-2:
     dico[tableauPlante[x][compteurDico]]=tableauPlante[x][compteurDico+1]
     compteurDico=compteurDico+2
 
 print("chargement en cours...");
 
+        # Récupération des informations de chaque capteur #
 while exit == 0:
     while choixInformationsValide == "oui":
         parametres ="v2/users/valmnt/devices/" + app + "/LuxValue?authorization="+ validtoken["access_token"]
@@ -162,6 +173,7 @@ while exit == 0:
         "L'humidite de l'air":ha['out'],
         }
 
+        # Si pas de capteurs alors on quitte le programme #
         if(lux['out'] == None):
             print("")
             print("Pas de capteur de lumière")
@@ -181,6 +193,7 @@ while exit == 0:
             print("")
             sys.exit()
             
+        # Affichage des informations selon l'option choisis #
         print("")
         choixInformations = input("Voulez vous voir les besoins de la plante(0), son état actuel(1) ou une description de la plante(2) ?")
         if choixInformations == "0":
@@ -208,6 +221,7 @@ while exit == 0:
         else:
             print("Cela ne fait pas partie des choix possibles.")
 
+        # Vérification de la poursuite ou non du programme #
         verifExit=0
         while verifExit == 0:
             choixInformationsValide = str(input("Voulez vous continuer à utiliser l'appli ?(oui ou non)"))
@@ -220,6 +234,7 @@ while exit == 0:
                 verifExit = 1
                 exit = 1
 
+        # Fermeture du programme #
 fichier.close()
 
 
